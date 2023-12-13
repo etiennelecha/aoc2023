ff4bf398ea50d493b1243f061a1016ae7868660a
Etienne Lechat <etienne@tpqube.com>
2023-12-13 11:19:00 +0100

    day 1213

---
+++ day12.py
@@ -0,0 +1,72 @@
+import re
+from functools import lru_cache
+from time import time
+
+W_REGEX = re.compile(r"\.+")
+
+with open("inputs/day12.txt") as f:
+    lines = f.read()
+start = time()
+
+@lru_cache
+def get_regex(i):
+    """
+    Returns a pattern which matches a group of one or more machines which has exactly the given length.
+    Uses an LRU cache so that we don't recompile the same patterns again and again
+    """
+    return re.compile(r"[\#\?]{%i}(\.|\?|$)" % i)
+
+@lru_cache
+def arrangement_counter(springs: str, pattern: tuple) -> int:
+    if not springs:
+        if pattern:
+            return 0
+        return 1
+    if not pattern:
+        if "#" in springs:
+            return 0
+        return 1
+    
+    removable_m = W_REGEX.match(springs)
+    if removable_m:
+        return arrangement_counter(springs[len(removable_m.group()) :], pattern)
+    arrangements = 0
+    # string start with ? or #
+    if springs[0] == "?":
+        arrangements += arrangement_counter(springs[1:], pattern)
+
+    curr_block = pattern[0]
+    possible_fit_m = get_regex(curr_block).match(springs)
+    if possible_fit_m:
+        remaining_blocks = pattern[1:]
+        # Assume all machines in this range are broken.
+        arrangements += arrangement_counter(
+            springs[len(possible_fit_m.group()) :], remaining_blocks
+        )
+
+    return arrangements
+
+
+ans1 = 0
+
+for line in lines.split("\n"):
+    springs, pattern = line.split(" ")
+    damaged = tuple([int(e) for e in pattern.split(",")])
+    arrangements = arrangement_counter(springs, damaged)
+    ans1 += arrangements
+
+ans2 = 0
+
+for i, line in enumerate(lines.split("\n")):
+    springs, pattern = line.split(" ")
+    springs = "?".join([springs for _ in range(5)])
+    pattern = ",".join([pattern for _ in range(5)])
+    damaged = tuple([int(d) for d in pattern.split(",")])
+    arrangements = arrangement_counter(springs, damaged)
+    ans2 += arrangements
+
+m, s = divmod(time() - start, 60)
+
+print(f"    \u2022 first part: {ans1}")
+print(f"    \u2022 second part: {ans2}")
+print(f"Done in {m:.0f}m{s:.4f}s")
---
+++ day13.py
@@ -0,0 +1,58 @@
+from time import time
+
+with open("inputs/day13.txt") as f:
+    whole_input = f.read()
+start = time()
+blocks = whole_input.split("\n\n")
+
+def is_reflection(start_idx, lines):
+    di = 0
+    l = len(lines)
+    while lines[start_idx - di] == lines[start_idx + 1 + di]:
+        if (start_idx - di) == 0 or (start_idx + 1 + di) == l - 1:
+            return start_idx + 1
+        di += 1
+    return 0
+
+def diff_count(line1, line2):
+    return len(list(filter(lambda t: t[0] != t[1],  zip(line1, line2))))
+
+def is_pseudo_reflection(start_idx, lines):
+    smidge = 1
+    di = 0
+    l = len(lines)
+    diff = diff_count(lines[start_idx - di], lines[start_idx + 1 + di])
+    while diff <= smidge:
+        smidge -= diff
+        if (start_idx - di) == 0 or (start_idx + 1 + di) == l - 1:
+            return start_idx + 1 if smidge == 0 else 0
+        di += 1
+        diff = diff_count(lines[start_idx - di], lines[start_idx + 1 + di])
+    return 0
+
+ans1 = 0
+ans2 = 0
+for block in blocks:
+    h_lines = block.split("\n")
+    m = len(h_lines)
+    v_lines = ["".join(line[i] for line in h_lines) for i in range(len(h_lines[0]))]
+    n = len(v_lines)
+    i = 0
+    j = 0
+    while i < m - 1:
+        ans2 += 100 * is_pseudo_reflection(i, h_lines)
+        if h_lines[i] == h_lines[i + 1]:
+            ans1 += 100 * is_reflection(i, h_lines)
+        i += 1
+    
+    while j < n - 1:
+        ans2 += is_pseudo_reflection(j, v_lines)
+        if v_lines[j] == v_lines[j + 1]:
+            ans1 += is_reflection(j, v_lines)
+        j += 1
+
+m, s = divmod(time() - start, 60)
+print(f"    \u2022 first part: {ans1}")
+print(f"    \u2022 second part: {ans2}")
+print(f"Done in {m:.0f}m{s:.4f}s")
+                
